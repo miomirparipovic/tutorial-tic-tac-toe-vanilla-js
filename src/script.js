a7f2a3b03cd7b2fc29167b68cb04dc20463b4ac6
Miomir Paripovic <deploy.friday@gmail.com>
2023-12-12 11:48:17 +0100

    Add win logic

--- src/script.js
+++ src/script.js
@@ -1,15 +1,31 @@
 const X_CLASS = "x";
 const O_CLASS = "o";
+const WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 let xTurn = true;
 let counter = 8;
 
 const cells = document.querySelectorAll(".cell");
+const board = document.getElementById("board");
 
-cells.forEach((cell) => {
-  cell.addEventListener("click", handleClick, {
-    once: true,
+startGame();
+
+function startGame() {
+  board.classList.add(X_CLASS);
+  cells.forEach((cell) => {
+    cell.addEventListener("click", handleClick, {
+      once: true,
+    });
   });
-});
+}
 
 function handleClick(e) {
   // place the mark
@@ -18,12 +34,18 @@ function handleClick(e) {
   placeSymbol(cell, currentClass);
 
   // check for win
+  if (checkForWin(currentClass)) {
+    console.log(`winner is ${currentClass}`);
+  }
 
   // check for draw
   checkForDraw();
 
   // switch turns
   xTurn = switchTurns(xTurn);
+
+  // set hover symbol based on turn
+  setSymbolHover();
 }
 
 function placeSymbol(cell, currentClass) {
@@ -39,6 +61,25 @@ function checkForDraw() {
   counter--;
 }
 
+function checkForWin(currentClass) {
+  return WIN_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cells[index].classList.contains(currentClass);
+    });
+  });
+}
+
 function switchTurns(turn) {
   return !turn;
+}
+
+function setSymbolHover() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(O_CLASS);
+
+  if (xTurn) {
+    board.classList.add(X_CLASS);
+  } else {
+    board.classList.add(O_CLASS);
+  }
 }
