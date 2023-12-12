a1eed559199c30e26767fdc89d14981635f7cfd3
Miomir Paripovic <deploy.friday@gmail.com>
2023-12-12 10:54:03 +0100

    Add check for draw, switch

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const X_CLASS = "x";
 const O_CLASS = "o";
-let xTurn = "true";
+let xTurn = true;
+let counter = 8;
 
 const cells = document.querySelectorAll(".cell");
 
@@ -11,18 +12,33 @@ cells.forEach((cell) => {
 });
 
 function handleClick(e) {
-  //   console.log("clicked", e);
   // place the mark
   const cell = e.target;
   const currentClass = xTurn ? X_CLASS : O_CLASS;
   placeSymbol(cell, currentClass);
 
   // check for win
+
   // check for draw
+  checkForDraw();
+
   // switch turns
+  xTurn = switchTurns(xTurn);
 }
 
 function placeSymbol(cell, currentClass) {
-  //   console.log("mark");
   cell.classList.add(currentClass);
+}
+
+function checkForDraw() {
+  if (!counter) {
+    let msgWindow = document.getElementById("message-wrapper");
+    msgWindow.classList.add("show");
+  }
+
+  counter--;
+}
+
+function switchTurns(turn) {
+  return !turn;
 }
