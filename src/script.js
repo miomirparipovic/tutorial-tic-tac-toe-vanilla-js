c73aea0e0839ad0403026b3fdf3025bf6cabee75
Miomir Paripovic <deploy.friday@gmail.com>
2023-12-12 10:35:32 +0100

    Add placeSymbol

--- src/script.js
+++ src/script.js
@@ -1,11 +1,28 @@
-const cells = document.querySelectorAll(".cell");
+const X_CLASS = "x";
+const O_CLASS = "o";
+let xTurn = "true";
 
-function handleClick(e) {
-  console.log("clicked", e);
-}
+const cells = document.querySelectorAll(".cell");
 
 cells.forEach((cell) => {
   cell.addEventListener("click", handleClick, {
     once: true,
   });
 });
+
+function handleClick(e) {
+  //   console.log("clicked", e);
+  // place the mark
+  const cell = e.target;
+  const currentClass = xTurn ? X_CLASS : O_CLASS;
+  placeSymbol(cell, currentClass);
+
+  // check for win
+  // check for draw
+  // switch turns
+}
+
+function placeSymbol(cell, currentClass) {
+  //   console.log("mark");
+  cell.classList.add(currentClass);
+}
