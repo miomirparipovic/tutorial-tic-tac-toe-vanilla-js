39b7514d3ed521d9e0e87f55e6910f6e5cf3a9e3
Miomir Paripovic <deploy.friday@gmail.com>
2023-12-12 10:19:02 +0100

    Add click event listener

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
+const cells = document.querySelectorAll(".cell");
 
+function handleClick(e) {
+  console.log("clicked", e);
+}
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleClick, {
+    once: true,
+  });
+});
